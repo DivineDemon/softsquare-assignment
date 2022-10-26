059d13b0fa94705f8437d34ef5a9b7915674eb51
DivineDemon <supame123@gmail.com>
2022-10-26 16:41:23 +0500

    BootstrapVue Setup! :sparkles:

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
+import { BootstrapVue } from "bootstrap-vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// BSV Plugin Initialized
+Vue.use(BootstrapVue);
+
+// Defining Environment
 Vue.config.productionTip = false;
 
 new Vue({
